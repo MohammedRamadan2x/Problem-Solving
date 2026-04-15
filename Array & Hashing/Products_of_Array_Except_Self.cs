48a43d68e9c80e376e984951e08698df865782b3
Mohammed Ramadan <mohamram115588@gmail.com>
2026-04-15 18:17:08 +0200

    Add another solution for 'Products of Array Except Self'
    Added an alternative solution using prefix and suffix arrays for the Product Except Self problem.

--- Array & Hashing/Products_of_Array_Except_Self.cs
+++ Array & Hashing/Products_of_Array_Except_Self.cs
@@ -16,3 +16,29 @@ public class Solution {
         return res;
     }
 }
+
+
+// Another Solution
+// ------------------------
+
+// public class Solution {
+//     public int[] ProductExceptSelf(int[] nums) {
+//         int n = nums.Length;
+//         int[] result = new int[n];
+//         int[] prefix = new int[n];
+//         int[] suffix = new int[n];
+
+//         prefix[0] = suffix[n - 1] = 1;
+
+//         for (int i = 1; i < n; ++i)
+//             prefix[i] = nums[i - 1] * prefix[i - 1];
+
+//         for (int i = n - 2; i >= 0; --i)
+//             suffix[i] = nums[i + 1] * suffix[i + 1];
+
+//         for (int i = 0; i < n; ++i)
+//             result[i] = prefix[i] * suffix[i];
+
+//         return result;
+//     }
+// }
